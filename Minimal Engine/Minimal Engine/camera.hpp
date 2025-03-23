5a74ba7a865ad5e9e73797f36450a49106dd65c7
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2025-03-23 19:55:41 -0400

    completed tutorial 14 https://www.youtube.com/watch?v=rvJHkYnAR3w&list=PL8327DO66nu9qYVKLDmdLW_84-yE4auCR&index=17

--- Minimal Engine/Minimal Engine/camera.hpp
+++ Minimal Engine/Minimal Engine/camera.hpp
@@ -9,13 +9,18 @@ namespace minimal
     class camera
     {
     public:
-        void set_othrographic_projection(float left, float right, float top, float bottom, float near, float far);
+        void set_orthographic_projection(float left, float right, float top, float bottom, float near, float far);
         void set_perspective_projection(float fovy, float aspect, float near, float far);
 
+        void set_view_direction(glm::vec3 position, glm::vec3 direction, glm::vec3 up = {0.0f, -1.0f, 0.0f});
+        void set_view_target(glm::vec3 position, glm::vec3 target, glm::vec3 up = {0.0f, -1.0f, 0.0f});
+        void set_view_YXZ(glm::vec3 position, glm::vec3 rotation);
+
         const glm::mat4& get_projection() const { return projection_matrix_; }
+        const glm::mat4& get_view() const { return view_matrix_; }
 
     private:
         glm::mat4 projection_matrix_{1.0f};
+        glm::mat4 view_matrix_{1.0f};
     };
-    
 }
