dd4dbaa72462dd204b56cea0234e4aafd089b2f1
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2025-03-24 22:35:59 -0400

    Tutorial 18 completed https://www.youtube.com/watch?v=wfh2N4u-nOU&list=PL8327DO66nu9qYVKLDmdLW_84-yE4auCR&index=21 Diffuse shading model implemented

--- Minimal Engine/Minimal Engine/model.hpp
+++ Minimal Engine/Minimal Engine/model.hpp
@@ -18,7 +18,6 @@ namespace minimal
             glm::vec3 normal{};
             glm::vec2 uv{};
 
-
             static std::vector<VkVertexInputBindingDescription> get_binding_descriptions();
             static std::vector<VkVertexInputAttributeDescription> get_attribute_descriptions();
 
--- Minimal Engine/Minimal Engine/simple_renderer_system.cpp
+++ Minimal Engine/Minimal Engine/simple_renderer_system.cpp
@@ -13,7 +13,7 @@ namespace minimal
     struct simple_push_constant_data
     {
         glm::mat4 transform{1.0f};
-        alignas(16) glm::vec3 color;
+        glm::mat4 normal_matrix{1.0f};
     };
 
     simple_renderer_system::simple_renderer_system(device& device, VkRenderPass render_pass) : device_{device}
@@ -65,8 +65,9 @@ namespace minimal
         for (auto& obj : game_objects)
         {
             simple_push_constant_data push{};
-            push.color = obj.color;
-            push.transform = projection_view * obj.transform.mat4();
+            auto model_matrix = obj.transform.mat4();
+            push.transform = projection_view * model_matrix;
+            push.normal_matrix = obj.transform.normal_matrix();
 
             vkCmdPushConstants(command_buffer,
                                pipeline_layout_,
