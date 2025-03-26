65e0fd2d3ecdc5ceac48409bdeaecce684de52c7
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2025-03-25 22:54:35 -0400

    Tutorial 24 completed https://www.youtube.com/watch?v=91-89b3wlSo&list=PL8327DO66nu9qYVKLDmdLW_84-yE4auCR&index=28 point light rendered using billboards

---
+++ Minimal Engine/shaders/point_light.vert
@@ -0,0 +1,34 @@
+﻿#version 450
+
+const vec2 OFFSETS[6] = vec2[](
+vec2(-1.0, -1.0),
+vec2(-1.0, 1.0),
+vec2(1.0, -1.0),
+vec2(1.0, -1.0),
+vec2(-1.0, 1.0),
+vec2(1.0, 1.0)
+);
+
+layout(location = 0) out vec2 fragOffset;
+
+layout(set = 0, binding = 0) uniform GlobalUbo {
+    mat4 projection;
+    mat4 view;
+    vec4 ambientColor;// w is light intensity
+    vec3 lightPosition;
+    vec4 lightColor;// w is light intensity
+} ubo;
+
+const float LIGHT_RADIUS = 0.05;
+
+void main() {
+    fragOffset = OFFSETS[gl_VertexIndex];
+    vec3 cameraWorldRight = { ubo.view[0][0], ubo.view[1][0], ubo.view[2][0] };
+    vec3 cameraWorldUp = { ubo.view[0][1], ubo.view[1][1], ubo.view[2][1] };
+
+    vec3 worldPostion = ubo.lightPosition.xyz
+    + LIGHT_RADIUS * fragOffset.x * cameraWorldRight
+    + LIGHT_RADIUS * fragOffset.y * cameraWorldUp;
+
+    gl_Position = ubo.projection * ubo.view * vec4(worldPostion, 1.0);
+}
--- Minimal Engine/src/first_app.cpp
+++ Minimal Engine/src/first_app.cpp
@@ -3,7 +3,8 @@
 #include "keyboard_movement_controller.hpp"
 #include "buffer.hpp"
 #include "camera.hpp"
-#include "simple_renderer_system.hpp"
+#include "systems/simple_renderer_system.hpp"
+#include "systems/point_light_system.hpp"
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -13,29 +14,36 @@
 #include <algorithm>
 #include <chrono>
 
-namespace minimal {
-    struct global_ubo {
-        glm::mat4 projection_view{1.0f};
+namespace minimal
+{
+    struct global_ubo
+    {
+        glm::mat4 projection{1.0f};
+        glm::mat4 view{1.0f};
         glm::vec4 ambient_color{0.1f, 0.1f, 0.1f, 0.2f}; // w is intensity
         glm::vec3 light_position{-1.0f};
         alignas(16) glm::vec4 light_color{1.0f}; // w is light intensity
     };
 
-    first_app::first_app() {
+    first_app::first_app()
+    {
         global_pool_ = descriptor_pool::builder(device_)
-                .setMaxSets(swap_chain::MAX_FRAMES_IN_FLIGHT)
-                .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, swap_chain::MAX_FRAMES_IN_FLIGHT)
-                .build();
+                       .setMaxSets(swap_chain::MAX_FRAMES_IN_FLIGHT)
+                       .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, swap_chain::MAX_FRAMES_IN_FLIGHT)
+                       .build();
 
         load_game_objects();
     }
 
-    first_app::~first_app() {
+    first_app::~first_app()
+    {
     }
 
-    void first_app::run() {
-        std::vector<std::unique_ptr<buffer> > ubo_buffers(swap_chain::MAX_FRAMES_IN_FLIGHT);
-        for (int i = 0; i < ubo_buffers.size(); i++) {
+    void first_app::run()
+    {
+        std::vector<std::unique_ptr<buffer>> ubo_buffers(swap_chain::MAX_FRAMES_IN_FLIGHT);
+        for (int i = 0; i < ubo_buffers.size(); i++)
+        {
             ubo_buffers[i] = std::make_unique<buffer>(
                 device_,
                 sizeof(global_ubo),
@@ -47,15 +55,16 @@ namespace minimal {
         }
 
         auto global_set_layout = descriptor_set_layout::builder(device_)
-                .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
-                .build();
+                                 .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
+                                 .build();
 
         std::vector<VkDescriptorSet> global_descriptor_sets(swap_chain::MAX_FRAMES_IN_FLIGHT);
-        for (int i = 0; i < swap_chain::MAX_FRAMES_IN_FLIGHT; i++) {
+        for (int i = 0; i < swap_chain::MAX_FRAMES_IN_FLIGHT; i++)
+        {
             auto buffer_info = ubo_buffers[i]->descriptorInfo();
             descriptor_writer(*global_set_layout, *global_pool_)
-                    .writeBuffer(0, &buffer_info)
-                    .build(global_descriptor_sets[i]);
+                .writeBuffer(0, &buffer_info)
+                .build(global_descriptor_sets[i]);
         }
 
         simple_renderer_system simple_renderer_system{
@@ -63,6 +72,13 @@ namespace minimal {
             renderer_.get_swap_chain_render_pass(),
             global_set_layout->getDescriptorSetLayout()
         };
+
+        point_light_system point_light_system{
+            device_,
+            renderer_.get_swap_chain_render_pass(),
+            global_set_layout->getDescriptorSetLayout()
+        };
+
         camera camera{};
         // camera.set_view_direction(glm::vec3(0.0f), glm::vec3(0.5f, 0.0f, 1.0f));
         camera.set_view_target(glm::vec3(-1.0f, -2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 2.5f));
@@ -74,11 +90,13 @@ namespace minimal {
 
         auto current_time = std::chrono::high_resolution_clock::now();
 
-        while (!window_.should_close()) {
+        while (!window_.should_close())
+        {
             glfwPollEvents();
 
             auto new_time = std::chrono::high_resolution_clock::now();
-            float frame_time = std::chrono::duration<float, std::chrono::seconds::period>(new_time - current_time).count();
+            float frame_time = std::chrono::duration<float, std::chrono::seconds::period>(new_time - current_time).
+                count();
             current_time = new_time;
 
             camera_controller.move_in_plane_xz(window_.get_glfw_window(), frame_time, viewer_object);
@@ -88,7 +106,8 @@ namespace minimal {
             // camera.set_othrographic_projection(-aspect, aspect, -1.0f, 1.0f, -1.0f, 1.0f);
             camera.set_perspective_projection(glm::radians(50.0f), aspect, 0.1f, 100.0f);
 
-            if (auto command_buffer = renderer_.begin_frame()) {
+            if (auto command_buffer = renderer_.begin_frame())
+            {
                 int frame_index = renderer_.get_frame_index();
 
                 frame_info frame_info{
@@ -102,13 +121,17 @@ namespace minimal {
 
                 // update
                 global_ubo ubo{};
-                ubo.projection_view = camera.get_projection() * camera.get_view();
+                ubo.projection = camera.get_projection();
+                ubo.view = camera.get_view();
                 ubo_buffers[frame_index]->writeToBuffer(&ubo);
                 ubo_buffers[frame_index]->flush();
 
                 // render
                 renderer_.being_swap_chain_render_pass(command_buffer);
+
                 simple_renderer_system.render_game_objects(frame_info);
+                point_light_system.render(frame_info);
+
                 renderer_.end_swap_chain_render_pass(command_buffer);
                 renderer_.end_frame();
             }
@@ -117,7 +140,8 @@ namespace minimal {
         vkDeviceWaitIdle(device_.get_device());
     }
 
-    void first_app::load_game_objects() {
+    void first_app::load_game_objects()
+    {
         std::shared_ptr<model> model = model::create_model_from_file(
             device_, "models\\flat_vase.obj");
 
